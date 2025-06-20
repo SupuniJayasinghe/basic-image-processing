8e3576fd981e962e069658c9dbd56f10cfc83b64
supunijayasinghe <supuninavodya695@gmail.com>
2025-06-20 23:48:30 +0530

    rotate image

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -13,6 +13,14 @@ def average_filter(img, ksize):
     return cv2.blur(img, (ksize, ksize))
 
 
+# Function to perform image rotation
+def rotate_image(img, angle):
+    h, w = img.shape[:2]
+    center = (w // 2, h // 2)
+    matrix = cv2.getRotationMatrix2D(center, angle, 1.0)
+    return cv2.warpAffine(img, matrix, (w, h))
+
+
 if __name__ == "__main__":
     img = cv2.imread("../images/image_1.png", cv2.IMREAD_GRAYSCALE)
 
@@ -30,3 +38,9 @@ if __name__ == "__main__":
     cv2.imwrite("../outputs/average_3x3.jpg", avg3)
     cv2.imwrite("../outputs/average_10x10.jpg", avg10)
     cv2.imwrite("../outputs/average_20x20.jpg", avg20)
+
+    rot45 = rotate_image(img, 45)
+    rot90 = rotate_image(img, 90)
+
+    cv2.imwrite("../outputs/rotated_45.jpg", rot45)
+    cv2.imwrite("../outputs/rotated_90.jpg", rot90)
