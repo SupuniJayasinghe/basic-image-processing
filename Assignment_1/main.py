8d8f98ff09fac4e714dbcc3b6a075886d09bab59
supunijayasinghe <supuninavodya695@gmail.com>
2025-06-20 23:33:53 +0530

    spatial averaging

--- Assignment_1/main.py
+++ Assignment_1/main.py
@@ -8,13 +8,25 @@ def reduce_intensity_levels(img, levels):
     return (img // factor) * factor
 
 
+# Function to perform Spatial Averaging
+def average_filter(img, ksize):
+    return cv2.blur(img, (ksize, ksize))
+
+
 if __name__ == "__main__":
     img = cv2.imread("../images/image_1.png", cv2.IMREAD_GRAYSCALE)
 
     reduced = reduce_intensity_levels(img, 4)
-
     cv2.imwrite("../outputs/reduced_4_levels.jpg", reduced)
-    print(np.unique(reduced))
+    # print(np.unique(reduced))
     reduced = reduce_intensity_levels(img, 8)
     cv2.imwrite("../outputs/reduced_8_levels.jpg", reduced)
-    print(np.unique(reduced))
+    # print(np.unique(reduced))
+
+    avg3 = average_filter(img, 3)
+    avg10 = average_filter(img, 10)
+    avg20 = average_filter(img, 20)
+
+    cv2.imwrite("../outputs/average_3x3.jpg", avg3)
+    cv2.imwrite("../outputs/average_10x10.jpg", avg10)
+    cv2.imwrite("../outputs/average_20x20.jpg", avg20)
